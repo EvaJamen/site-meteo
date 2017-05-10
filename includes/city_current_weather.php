e7b81d6e483abd7bac88a0080cc12923cf7c49e8
EvaJamen <jamen.eva@gmail.fr>
2017-05-10 16:29:46 +0200

    Create the home page and the city current weather page

---
+++ includes/city_current_weather.php
@@ -0,0 +1,32 @@
+<?php
+	//API Current weather data
+	include 'current_weather.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>City current weather</title>
+		<link rel="stylesheet" href="../src/css/style.css" />
+	</head>
+	<body>
+		<!-- HEADER -->
+		<header>
+			<section id="topBar">
+				<nav class="container">
+					<ul class="menu">
+						<li><a href="../index.php" title="menu">HOME</a></li>
+						<li><a href="#" title="menu">CITY CURRENT WEATHER</a></li>
+					</ul>
+				</nav>
+			</section>
+		</header>	
+		
+		<!-- FOOTER -->
+		<footer>
+		</footer>
+		
+		<script src="../src/js/script.js"></script>
+	</body>
+</html>
--- index.php
+++ index.php
@@ -1,18 +1,25 @@
 <?php
-	//API Current weather data
-	include 'includes/current_weather.php';
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
-		<title>Acceuil</title>
+		<title>Home</title>
 		<link rel="stylesheet" href="src/css/style.css" />
 	</head>
 	<body>
 		<!-- HEADER -->
 		<header>
+			<section id="topBar">
+				<nav class="container">
+					<ul class="menu">
+						<li><a href="#" title="menu">HOME</a></li>
+						<li><a href="includes/city_current_weather.php" title="menu">CITY CURRENT WEATHER</a></li>
+					</ul>
+				</nav>
+			</section>
 		</header>	
 		
 		<!-- FOOTER -->
