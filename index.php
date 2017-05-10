ea328b2ceb48420e385c7a02dfe52e1bee713a0a
EvaJamen <jamen.eva@gmail.fr>
2017-05-10 16:08:23 +0200

    Create template

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+	//API Current weather data
+	include 'includes/current_weather.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Acceuil</title>
+		<link rel="stylesheet" href="src/css/style.css" />
+	</head>
+	<body>
+		<!-- HEADER -->
+		<header>
+		</header>	
+		
+		<!-- FOOTER -->
+		<footer>
+		</footer>
+		
+		<script src="src/js/script.js"></script>
+	</body>
+</html>
