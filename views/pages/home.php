76e8c29f91dd95a80dd934a11a918a94c0c65131
EvaJamen <eva.jamen@hetic.net>
2017-05-13 19:13:40 +0200

    Continue CSS

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,12 +1,32 @@
 <section class="blocks">
-			<div class="col-md-12 block1">TEST</div>
-			<div class="col-md-3 block2">TEST 2</div>
-			<div class="col-md-3 block3">TEST 3</div>
-			<div class="col-md-3 block4">TEST 4</div>
-			<div class="col-md-3 block5">TEST 5</div>
+			
+			<div class="col-md-12 block1">
+				<div id="impact">Welcome ! Find oll you want about weather !</div>
+			</div>
+			
+			<div class="col-md-4 block2">
+				<section id="sidebar" class="column">
+        <img src="images/portrait.png" alt="portrait">
+        <span>All of cities</span>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at ligula diam. Donec a nibh sit amet justo posuere convallis. Curabitur suscipit consectetur nisl, vel mattis sem tempor quis. Aenean enim lectus, aliquet sed dignissim vel, laoreet et justo. Nulla turpis nulla, dapibus in porttitor sit amet, luctus ac erat.</p>
+      </section>
+			
+			</div>
+			
+			<div class="col-md-4 block3">
+			<section id="sidebar" class="column">
+        <img src="images/chat.png" alt="compétences">
+        <span>Your geolocation</span>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at ligula diam. Donec a nibh sit amet justo posuere convallis. Curabitur suscipit consectetur nisl, vel mattis sem tempor quis. Aenean enim lectus, aliquet sed dignissim vel, laoreet et justo. Nulla turpis nulla, dapibus in porttitor sit amet, luctus ac erat.</p>
+      </section>
+			</div>
+			
+			<div class="col-md-4 block4">
+			<section id="sidebar" class="column">
+        <img src="images/screw.png" alt="services">
+        <span>And more...</span>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at ligula diam. Donec a nibh sit amet justo posuere convallis. Curabitur suscipit consectetur nisl, vel mattis sem tempor quis. Aenean enim lectus, aliquet sed dignissim vel, laoreet et justo. Nulla turpis nulla, dapibus in porttitor sit amet, luctus ac erat.</p>
+      </section>
+      <div class="clearBoth"></div>
+			</div>
 </section>
-
-
-<p>
-	Our website is to inform you about all cases of weather, in the world. 
-</p>
